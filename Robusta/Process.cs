511917564c9cb0597235814814aa909d76fd4ba7
Devran Eroğul <devran@gmail.com>
2023-10-11 15:17:30 +0300

    Initial commit

---
+++ Robusta/Process.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Robusta
+{
+
+    public class ProcessStartRequest
+    {
+        public string runOnce { get; set; }
+        public string runImmediately { get; set; }
+        public string name { get; set; }
+        public string worker { get; set; }
+        public Dictionary<string, string> values { get; set; }
+    }
+
+    public class ProcessStartResponse
+    {
+        public string robustaSpid { get; set; }
+        
+        public string message { get; set; }
+    }
+
+    public class ProcessStatusResponse
+    {
+        public string status { get; set; }
+        public string message { get; set; }
+    }
+    
+    public class Process
+    {
+        public static string _robustaUsername = "devran.erogul";
+        public static string _robustaPassword = "KKKKKKKKPassword";
+       
+        public static Dictionary<string, object> Start(List<Dictionary<string, object>> parameters)
+        {
+            var result = new Dictionary<string, object>();
+            var List = new List<Dictionary<string, object>>();
+            result["Error"] = "";
+
+
+            using (var client = new HttpClient())
+            {
+                var uri = $"{parameters[0]["uri"].ToString()}/scheduler/connector-api/process/start";
+
+                var req = new ProcessStartRequest
+                {
+                    name = parameters[2]["name"].ToString(),
+                    runImmediately = "Y",
+                    runOnce = "Y",
+                    worker = parameters[1]["worker"].ToString()
+                };
+
+                // Authorization
+                var authToken = Encoding.ASCII.GetBytes($"{_robustaUsername}:{_robustaPassword}");
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(authToken));
+
+                req.values = JsonConvert.DeserializeObject<Dictionary<string, string>>( @"{" + parameters[3]["values"].ToString() + "}");
+
+                var json = JsonConvert.SerializeObject(req);
+                var data = new StringContent(json, Encoding.UTF8, "application/json");
+                
+                var response = client.PostAsync(uri, data).GetAwaiter().GetResult();
+
+                var s = JsonConvert.DeserializeObject<ProcessStartResponse>(response.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+                
+                List.Add(new Dictionary<string, object> { { "Result", s.robustaSpid } });
+                
+                result["List"] = List;
+
+            }
+
+            return result;
+        }
+
+        public static Dictionary<string, object> Status(List<Dictionary<string, object>> parameters)
+        {
+            var result = new Dictionary<string, object>();
+            var List = new List<Dictionary<string, object>>();
+            result["Error"] = "";
+
+
+            using (var client = new HttpClient())
+            {
+                var id = parameters[1]["id"].ToString();
+                var uri = $"{parameters[0]["uri"].ToString()}/scheduler/connector-api/process/status/{id}";
+
+                // Authorization
+                var authToken = Encoding.ASCII.GetBytes($"{_robustaUsername}:{_robustaPassword}");
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(authToken));
+           
+                var response = client.GetAsync(uri).GetAwaiter().GetResult();
+
+                var s = JsonConvert.DeserializeObject<ProcessStatusResponse>(response.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+
+                List.Add(new Dictionary<string, object> { { "Result", s.message } });
+
+                result["List"] = List;
+            }
+
+            return result;
+
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            startProcess();
+            //queryProcess();
+        }
+
+        static void startProcess()
+        {
+
+            // Assembly path
+            var assembly = Assembly.LoadFrom(@"C:\Users\DevranErogul\source\repos\Robusta\Robusta\bin\Debug\Robusta.dll");
+
+            // Namespace ve Class
+            var type = assembly.GetType("Robusta.Process");
+            var method = type.GetMethod("Start");
+            var paramss = new object[1];
+            var paramlist = new List<Dictionary<string, object>>();
+
+            // Robusta host url
+            paramlist.Add(new Dictionary<string, object>
+            {
+                {"uri", "https://prod.robusta.ai:8443"},
+            });
+
+            paramlist.Add(new Dictionary<string, object>
+            {
+                {"worker", "afd54558-8ebd-4655-97a7-5afb808ebc68" },
+            });
+            paramlist.Add(new Dictionary<string, object>
+            {
+                {"name", "xpodatest2" },
+            });
+            paramlist.Add(new Dictionary<string, object>
+            {
+                {"values", @"{""URL"": ""https://partner.xpodacloud.com/"",""UserName"": ""DevranE"",""Password"" : ""DevranE""}" },
+            });
+
+            paramss[0] = paramlist;
+
+            var obj = Activator.CreateInstance(type);
+
+            // Metodu cagiriyoruz
+            var rs = method.Invoke(obj, paramss) as Dictionary<string, object>;
+
+            List<Dictionary<string, object>> x = (List<Dictionary<string, object>>)rs["List"];
+
+            foreach (var item in x)
+            {
+                Console.Write(item["Result"].ToString());
+            }
+
+
+            Console.ReadKey();
+        }
+
+        static void queryProcess()
+        {
+            // Assembly path
+            var assembly = Assembly.LoadFrom(@"C:\Users\DevranErogul\source\repos\Robusta\Robusta\bin\Debug\Robusta.dll");
+
+            // Namespace ve Class
+            var type = assembly.GetType("Robusta.Process");
+            var method = type.GetMethod("Status");
+            var paramss = new object[1];
+            var paramlist = new List<Dictionary<string, object>>();
+
+            // Robusta host url
+            paramlist.Add(new Dictionary<string, object>
+            {
+                {"uri", "https://prod.robusta.ai:8443"},
+            });
+
+            paramlist.Add(new Dictionary<string, object>
+            {
+                {"id", "4ac04650-23cb-41e9-99fc-202a34136b86" },
+            });
+
+         
+            paramss[0] = paramlist;
+
+            var obj = Activator.CreateInstance(type);
+
+            // Metodu cagiriyoruz
+            var rs = method.Invoke(obj, paramss) as Dictionary<string, object>;
+
+            List<Dictionary<string, object>> x = (List<Dictionary<string, object>>)rs["List"];
+
+            foreach (var item in x)
+            {
+                Console.Write(item["Result"].ToString());
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
